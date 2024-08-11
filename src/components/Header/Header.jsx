4826a65e6fcba7a69a221450cb9010ce8b7b8d2b
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-11 00:48:20 -0500

    feat: create components header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>COLOMBIA</h1>
+      <h2>Datos & Curiosidades</h2>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/views/ColombiaDash.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+
+const ColombiaDash = () => {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+};
+
+export default ColombiaDash;
