64d3455e0622d260594b890120d96d52f8171c8a
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-11 11:48:01 -0500

    feat: create component layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import "./Layout.css";
+
+const Layout = ({ children }) => {
+  return <div className="layout-container">{children}</div>;
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
