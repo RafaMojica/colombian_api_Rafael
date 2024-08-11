51fead3d62251dd191a18f92ca7fdacd80517dfa
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-11 18:32:14 -0500

    chore: add class html element

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <h1>COLOMBIA</h1>
-      <h2>Datos & Curiosidades</h2>
+      <h1 className="title-main">COLOMBIA</h1>
+      <h2 className="title-secondary">Datos & Curiosidades</h2>
     </header>
   );
 };
